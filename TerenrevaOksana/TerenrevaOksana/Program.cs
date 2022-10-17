b0cb44bc1292b7a14e44a3e203624c424a6830df
TerentevaOksana <113889184+TerentevaOksana@users.noreply.github.com>
2022-10-17 10:07:35 +0300

    Add files via upload

---
+++ TerenrevaOksana/TerenrevaOksana/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int v = n % 100 / 10;
+            int x;
+
+            if (1 <= n && n <= 999 && v != 0)
+            {
+                x = ((n % 100 / 10) * 10) + (n / 100) + ((n % 10) * 100);
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine("Неверное число");
+            }
+        }
+    }
+}
